e81502797da9e1fae4ecbaf7a03d71443f96eb57
Diana Patiño <e.narvaez.paz@gmail.com>
2025-03-06 23:47:16 -0500

    Class docente V1

--- Practica1/Docente.py
+++ Practica1/Docente.py
@@ -1,35 +1,3 @@
-''' Integrantes: 
-    Maria de los Angeles Flores
-    Luis Alejandro Gordillo
-    Leslie Alejandra Guerrero 
-    Diana Marcela Patiño
-'''
-
-from Persona import Persona
-
-class Docente(Persona):
-    def _init_(self, nombre, edad, direccion, curso):
-        super()._init_(nombre, edad, direccion)
-        if not isinstance(curso, str) or not curso.strip():
-            raise ValueError("El curso debe ser un curso válido")
-        self.__curso = curso
-    
-    def get_curso(self):
-        return self.__curso
-    
-    def set_curso(self, curso):
-        if not isinstance(curso, str) or not curso.strip():
-            raise ValueError("El curso debe ser un curso válido")
-        self.__curso = curso
-    
-    def _str_(self):
-        return f"Docente: {super()._str()} - Curso: {self._curso}"''' Integrantes: 
-    Maria de los Angeles Flores
-    Luis Alejandro Gordillo
-    Leslie Alejandra Guerrero 
-    Diana Marcela Patiño
-'''
-
 from Persona import Persona
 
 class Docente(Persona):
