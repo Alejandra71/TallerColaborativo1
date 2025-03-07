654fe02c8da857896d8bb2ec86526f3b12e01f85
Angeles <maria.fi1701@gmail.com>
2025-03-06 21:34:20 -0500

    revision estudiante v1

--- Practica1/Estudiante.py
+++ Practica1/Estudiante.py
@@ -1,43 +1,28 @@
-from Curso import Curso
+''' Integrantes: 
+    Maria de los Angeles Flores
+    Luis Alejandro Gordillo
+    Leslie Alejandra Guerrero 
+    Diana Marcela Patiño
+    
+  '''   
+from Persona import Persona
+from curso import Curso 
 
-class Estudiante:
+class Estudiante(Persona):
     def __init__(self, nombre, edad, direccion, curso):
-        if not isinstance(curso, Curso):
-            raise TypeError("El curso debe ser una instancia de la clase Curso.")
-        self.nombre = nombre
-        self.edad = edad
-        self.direccion = direccion
-        self.curso = curso
-
-    # Métodos GET
-    def get_nombre(self):
-        return self.nombre
-
-    def get_edad(self):
-        return self.edad
-
-    def get_direccion(self):
-        return self.direccion
+        super().__init__(nombre, edad, direccion)
+        self.__curso = curso 
+    
 
     def get_curso(self):
-        return self.curso
-
-    # Métodos SET
-    def set_nombre(self, nuevo_nombre):
-        self.nombre = nuevo_nombre
-
-    def set_edad(self, nueva_edad):
-        self.edad = nueva_edad
-
-    def set_direccion(self, nueva_direccion):
-        self.direccion = nueva_direccion
-
-    def set_curso(self, nuevo_curso):
-        if not isinstance(nuevo_curso, Curso):
-            print("El curso asignado no es válido.")
-            return
-        self.curso = nuevo_curso
-
-    # Método para mostrar información del estudiante
+        return self.__curso
+    
+ 
+    def set_curso(self, curso):
+        if isinstance(curso, Curso):
+            self.__curso = curso
+        else:
+            raise ValueError("El curso debe ser una instancia de la clase Curso")
+    
     def __str__(self):
-        return f"Nombre: {self.nombre}, \n Edad: {self.edad}, \n Dirección: {self.direccion}, \n Curso: {self.curso.nombre}"
+        return f"{super().__str__()}\nMatriculado en: {self.__curso}"
